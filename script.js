08e02d2b99f4850b192237fedc83c500477528ec
Aces91 <schillertony@hotmail.de>
2022-11-06 09:10:51 +0100

    import picture, name and id in the Detailed Card

--- script.js
+++ script.js
@@ -3,7 +3,17 @@ let currentPokemon;
 async function loadPokemon() {
     let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
     let response = await fetch(url);
-    let respJson = await response.json();
+    currentPokemon = await response.json();
 
-    console.log(respJson)
+    console.log(currentPokemon)
+    renderPokeInfo();
+}
+
+function renderPokeInfo() {
+    let name = currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1); // first character of string to upper Case.
+    let img = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+
+    document.getElementById('pokeName').innerHTML = name; // Output the name
+    document.getElementById('pokeImg').src = img
+    document.getElementById('pokeId').innerHTML = "# " + currentPokemon['id'];
 }
