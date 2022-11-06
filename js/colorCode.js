f3738761952c0978540c3e10a18315788c07b623
Aces91 <schillertony@hotmail.de>
2022-11-06 11:59:55 +0100

    import type and type-color code.

---
+++ js/colorCode.js
@@ -0,0 +1,115 @@
+let colorCode = [{
+    'type': 'normal',
+    'color-light': 'cfcfc3',
+    'color-dark': 'bbbbaa',
+    'color-dark-plus': '707066'
+},
+{
+    'type': 'fighting',
+    'color-light': 'cf887c',
+    'color-dark': 'bb5544',
+    'color-dark-plus': '703328'
+},
+{
+    'type': 'flying',
+    'color-light': 'b5d9ff',
+    'color-dark': '6699ff',
+    'color-dark-plus': '5a7999'
+},
+{
+    'type': 'poisen',
+    'color-light': 'b486dc',
+    'color-dark': 'aa5599',
+    'color-dark-plus': '59317b'
+},
+{
+    'type': 'ground',
+    'color-light': 'c09d74',
+    'color-dark': 'ddbb55',
+    'color-dark-plus': '634522'
+},
+{
+    'type': 'rock',
+    'color-light': 'cfc393',
+    'color-dark': 'bbaa66',
+    'color-dark-plus': '70663d'
+},
+{
+    'type': 'bug',
+    'color-light': 'b2d369',
+    'color-dark': 'aabb22',
+    'color-dark-plus': '577319'
+},
+{
+    'type': 'ghost',
+    'color-light': '997b9a',
+    'color-dark': '6666bb',
+    'color-dark-plus': '422843'
+},
+{
+    'type': 'steel',
+    'color-light': 'c3c3cf',
+    'color-dark': 'aaaabb',
+    'color-dark-plus': '666670'
+},
+{
+    'type': 'fire',
+    'color-light': 'ff7a60',
+    'color-dark': 'f4563a',
+    'color-dark-plus': '992710'
+},
+{
+    'type': 'water',
+    'color-light': '6bb9eb',
+    'color-dark': '3399ff',
+    'color-dark-plus': '1a5d88'
+},
+{
+    'type': 'grass',
+    'color-light': '91d08b',
+    'color-dark': '77cc55',
+    'color-dark-plus': '3a7036'
+},
+{
+    'type': 'electric',
+    'color-light': 'ffe64c',
+    'color-dark': 'ffcc33',
+    'color-dark-plus': '998400'
+},
+{
+    'type': 'psychic',
+    'color-light': 'ff91a6',
+    'color-dark': 'ff5599',
+    'color-dark-plus': '993b4c'
+},
+{
+    'type': 'ice',
+    'color-light': '9dddd2',
+    'color-dark': '83d4ef',
+    'color-dark-plus': '457c73'
+},
+{
+    'type': 'dragon',
+    'color-light': '889ad1',
+    'color-dark': '17766ee',
+    'color-dark-plus': '334372'
+},
+{
+    'type': 'dark',
+    'color-light': '837676',
+    'color-dark': '775544',
+    'color-dark-plus': '2e2929'
+},
+{
+    'type': 'fairy',
+    'color-light': 'f1b0ed',
+    'color-dark': 'ffaaff',
+    'color-dark-plus': '8d558a'
+},
+{
+    'type': 'unknow',
+    'color-light': '95bcb1',
+    'color-dark': '68A090',
+    'color-dark-plus': '3e6056'
+},
+];
--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 let currentPokemon;
+let pokemons = 25;
 
 async function loadPokemon() {
-    let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
-    let response = await fetch(url);
-    currentPokemon = await response.json();
+  let url = `https://pokeapi.co/api/v2/pokemon/bulbasaur`;
+  let response = await fetch(url);
+  currentPokemon = await response.json();
 
-    console.log(currentPokemon)
-    renderPokeInfo();
+  console.log(currentPokemon);
+  renderPokeInfo();
 }
 
 function renderPokeInfo() {
-    let name = currentPokemon['name'].charAt(0).toUpperCase() + currentPokemon['name'].slice(1); // first character of string to upper Case.
-    let img = currentPokemon['sprites']['other']['official-artwork']['front_default'];
+  let name =
+    currentPokemon["name"].charAt(0).toUpperCase() +
+    currentPokemon["name"].slice(1); // first character of string to upper Case.
+  let img =
+    currentPokemon["sprites"]["other"]["official-artwork"]["front_default"];
+    
 
-    document.getElementById('pokeName').innerHTML = name; // Output the name
-    document.getElementById('pokeImg').src = img
-    document.getElementById('pokeId').innerHTML = "# " + currentPokemon['id'];
+  document.getElementById("pokeName").innerHTML = name; // Output the name
+  document.getElementById("pokeImg").src = img;
+  document.getElementById("pokeId").innerHTML = "#" + currentPokemon["id"];
+  
+  pokemonType();
+}
+
+function pokemonType() {
+    let type1 = currentPokemon['types']['0']['type']['name'];
+    document.getElementById("pokeType1").innerHTML = type1;
+
+    for (let i = 0; i < colorCode.length; i++) {
+        let type = colorCode[i]['type'];
+
+        //TODO - keine abbruch bedingung programmiert
+        if (type1 == type) {
+            document.getElementById('pokeType1').style = `background-color: #${colorCode[i]['color-dark']};`;
+        } 
+    }
 }
